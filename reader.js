b404b3db538c4d8e67955ab94bf101c5bb113300
GalvinID <68190778+GalvinID@users.noreply.github.com>
2020-11-04 10:50:22 +0700

    Add files via upload

---
+++ reader.js
@@ -0,0 +1,26 @@
+let request = require('request');
+const log4js = require('log4js')
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+log4js.configure({
+  appenders: {
+    multi: { type: 'multiFile', base: 'logs/', property: 'categoryName', extension: '.html' }
+  },
+  categories: {
+    default: { appenders: [ 'multi' ], level: 'debug' }
+  }
+});
+
+const data = log4js.getLogger('data');
+console.log('Growtopia/serve_data.php reader coded by GalvinID credit to Clayne (C)')
+readline.question('Target IP : ', ip => {
+    request.post(`http://${ip}/growtopia/server_data.php`, function(err, response, body) {
+    data.info(`${response && response.statusCode}\n${body}`)//saved in /logs (auto save)
+	console.log(`${response && response.statusCode}\n${body}`)//will shown in terminal
+	console.log('html/css copied successfully')
+    readline.close();
+  });
+})
